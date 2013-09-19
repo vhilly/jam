82ba8dc65e32284fb18f1ccd2675c685c082c1b6
vhilly <vhilly@imperium.ph>
2013-09-19 17:48:13 +0800

    altered status and created_at

--- protected/views/tickets/admin.php
+++ protected/views/tickets/admin.php
@@ -5,6 +5,11 @@
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
 'id'=>'tickets-grid',
 'dataProvider'=>$model->search(),
+
+'afterAjaxUpdate'=>"function() {
+  jQuery('#Tickets_created_at').datepicker({'format':'yyyy-mm-dd','language':'en','weekStart':0});
+}",
+
 'filter'=>$model,
 'columns'=>array(
 		'tkt_no',
@@ -14,8 +19,22 @@
 		array('name'=>'passenger_type_id','value'=>'$data->passengerType->name','filter'=>CHtml::listData(PassengerTypes::model()->findAll(),'id','name')),
 		array('name'=>'ticket_type_id','value'=>'$data->ticketType->name','filter'=>CHtml::listData(TicketTypes::model()->findAll(),'id','name')),
 		'amt',
-		array('name'=>'status','value'=>'$data->status ==1 ? "Open" : "Closed"','filter'=>array('1'=>'Valid','2'=>'Canceled')),
-		'created_at',
+		array('name'=>'status','value'=>'$data->status ==1 ? "Valid" : "Cancelled"','filter'=>array('1'=>'Valid','2'=>'Canceled')),
+
+		array(
+                  'name'=>'created_at',
+                        'filter'=>$this->widget('bootstrap.widgets.TbDatePicker', array(
+                          'model'=>$model,
+                          'options'=>array('format'=>'yyyy-mm-dd'),
+                          'htmlOptions' => array(
+                            'id' => 'Tickets_created_at'
+                          ),
+                         'attribute'=>'created_at'),
+                        true),
+                        'sortable'=>true,
+                ),
+
+#		'created_at',
 #		'created_by',
 		array('name'=>'created_by','value'=>'$data->user->username','filter'=>CHtml::listData(Users::model()->findAll(),'id','username')),
 array(
